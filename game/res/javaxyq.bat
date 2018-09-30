788aaa5ff996adda21ab7b5250bbb004c6d867f7
liulei <lei.liu@datatist.com>
2018-09-30 11:23:22 +0800

    添加 *nix运行

--- game/res/javaxyq.bat
+++ game/res/javaxyq.bat
@@ -1,5 +1,6 @@
 @echo off
 set myclasspath=lib/*;game-1.5.jar
 
-java -Xmx128m -classpath %myclasspath%  com.javaxyq.core.DesktopApplication
+java -Xmx128m -classpath ./lib/*;./game-1.5.jar  com.javaxyq.core.DesktopApplication
+
 pause
---
+++ game/res/javaxyq.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -Xmx128m -classpath ./lib/*:./game-1.5.jar  com.javaxyq.core.DesktopApplication
